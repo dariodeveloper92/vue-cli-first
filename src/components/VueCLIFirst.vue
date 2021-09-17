1399dd5fea145332436377abda7d3f599e0c2940
Dario <dario.defrancisci92@gmail.com>
2021-09-17 17:55:57 +0200

    creato scaffolding

---
+++ src/components/VueCLIFirst.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+      <h1>La mia prima app con Vue CLI!</h1>
+    <img alt="deadpool img" src="./assets/img_deadpool.jpg">
+    <VueCliFirst msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import VueCliFirst from './components/VueCliFirst.vue'
+
+
+export default {
+  name: 'App',
+  components: {
+    VueCliFirst
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
